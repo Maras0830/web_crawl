5ff6abb02cb6a1eee6e0a27d3f4317e7b1e41dbd
maraschen <maraschen@4gamers.com.tw>
2016-05-11 13:56:03 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # web_crawl
-web crawler 網路爬蟲, 在這是以爬 steam 為例
+web crawler - 網路爬蟲.  
+
+You can get more website information by crawler
+See more about web crawler [wiki](https://en.wikipedia.org/wiki/Web_crawler)  
+
+你可以使用爬蟲取得更多的網站資訊
+關於爬蟲的說明請看 [wiki](https://en.wikipedia.org/wiki/Web_crawler)  
+
+This project with [Steam](http://store.steampowered.com) as sample.
+這專案已 [Steam](http://store.steampowered.com) 為範例
+
+## Install and Usage - 安裝及使用教學
+
+Install and usage see [Codingweb](http://codingweb.tw/2016/05/11/python3-網路爬蟲教學/)  
+安裝及使用教學 請看 [Codingweb](http://codingweb.tw/2016/05/11/python3-網路爬蟲教學/)  
+
+## Notice - 注意
+
+This project can run on python3.x , python2.x can not run.
+
+此專案使用版本僅限於 python3.x , python2.x將無法使用
---
+++ web_crawl.py
@@ -0,0 +1,53 @@
+import urllib.request
+from optparse import OptionParser
+from bs4 import BeautifulSoup
+import sys
+
+#說明文件
+def usage():
+	print (''' \033[92m	crawl from to get something. \n
+	usage : python3 web_crawl.py [-u] [User]
+
+	-h : help
+	-u : User
+
+	By Maras. \033[0m''')
+	sys.exit()
+
+#取得參數輸入
+def get_parameters():
+	global appId
+	appId = None
+	optp = OptionParser(add_help_option=False,epilog="web_crawl")
+	optp.add_option("-a","--AppId",dest="appId",help="-u Steam App Game ID")
+	optp.add_option("-h","--help",dest="help",action='store_true',help="help you")
+	opts, args = optp.parse_args()
+	if opts.help:
+		usage()
+
+	if opts.appId is not None:
+		appId = opts.appId
+
+#開始
+if __name__ == '__main__':
+	if len(sys.argv) < 1:
+		usage()
+	get_parameters()
+	url = None
+	if appId is not None:
+		url = "http://store.steampowered.com/app/" + appId
+	else: 
+		print ('Please input -a or -h')
+
+	if url is not None:
+		#設置假的瀏覽器資訊
+		headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
+		#發送請求
+		req = urllib.request.Request(url = url,headers=headers)
+		page = urllib.request.urlopen(req)
+		contentBytes = page.read()
+		#進行分割
+		soup = BeautifulSoup(str(contentBytes), "html.parser")
+		matches = soup.find_all("div", { "class" : "game_purchase_price" })
+		#將資訊顯示出來
+		print(matches[0].string);
